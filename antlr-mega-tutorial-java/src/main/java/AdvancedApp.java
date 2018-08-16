e0bd7d5f5517f7bc4af6e87b5d3f4c3b6e25e2da
Juraj Martinka <jumarko@gmail.com>
2018-08-16 21:22:42 +0200

    Advanced. Chapter 26 - complete MarkupVisitor for parsing elements and tags.
    It could still be refactored though.

--- antlr-mega-tutorial-java/src/main/java/AdvancedApp.java
+++ antlr-mega-tutorial-java/src/main/java/AdvancedApp.java
@@ -7,7 +7,10 @@ public class AdvancedApp {
     public static void main(String[] args) {
         final CodePointCharStream inputStream =
                 CharStreams.fromString("I would like to [b][i]empahsize[/i][/b] this and [u]underline[b]that[/b][/u]");
-        final MarkupLexer markupLexer = new MarkupLexer(inputStream);
+        // another example
+        final CodePointCharStream inputStream2 =
+                CharStreams.fromString("I would like to [u]underline [b] that [/b] [/u]. Let's quote: [quote author=\"john\"]You're wrong! [/quote]");
+        final MarkupLexer markupLexer = new MarkupLexer(inputStream2);
         final MarkupParser markupParser = new MarkupParser(new CommonTokenStream(markupLexer));
         final MarkupParser.FileContext fileContext = markupParser.file();
         final MarkupVisitor markupVisitor = new MarkupVisitor();
--- antlr-mega-tutorial-java/src/main/java/MarkupVisitor.java
+++ antlr-mega-tutorial-java/src/main/java/MarkupVisitor.java
@@ -10,8 +10,55 @@ public class MarkupVisitor extends MarkupParserBaseVisitor<String> {
 
     @Override
     public String visitContent(MarkupParser.ContentContext ctx) {
-        System.out.println(ctx.TEXT().getText());
+        return ctx.getText();
+    }
 
-        return visitChildren(ctx);
+    @Override
+    public String visitElement(MarkupParser.ElementContext ctx) {
+        if (ctx.parent instanceof MarkupParser.FileContext) {
+            if (ctx.content() != null) {
+                System.out.print(visitContent(ctx.content()));
+            } else if (ctx.tag() != null) {
+                System.out.print(visitTag(ctx.tag()));
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public String visitTag(MarkupParser.TagContext ctx) {
+        final StringBuilder text = new StringBuilder();
+        String start = "";
+        String end = "";
+
+        switch (ctx.ID(0).getText()) {
+            case "b":
+                start = end = "**";
+                break;
+            case "u":
+                start = end = "*";
+                break;
+            case "quote":
+                // TODO: check the attribute name is actually "author"?
+                final String attributeValue = ctx.attribute().STRING().getText();
+                // trim the leading and trailing quote
+                final String author = attributeValue.substring(1, attributeValue.length() - 1);
+                start = System.lineSeparator() + "> ";
+                end = System.lineSeparator() + "> -" + author;
+                break;
+        }
+
+        text.append(start);
+        for (MarkupParser.ElementContext element : ctx.element()) {
+            if (element.content() != null) {
+                text.append(visitContent(element.content()));
+            } else if (element.tag() != null) {
+                text.append(visitTag(element.tag()));
+            } 
+        }
+        text.append(end);
+
+        return text.toString();
     }
 }
